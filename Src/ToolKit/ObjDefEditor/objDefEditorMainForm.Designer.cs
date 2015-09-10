38d8bb7fefcd701ee27187db2a24cb9abed45dbc
Alexandre Polosky <alexandre.polosky@amtrustgroup.com>
2015-09-10 16:34:36 -0400

    More fun things for objdef edit

--- Src/ToolKit/ObjDefEditor/objDefEditorMainForm.Designer.cs
+++ Src/ToolKit/ObjDefEditor/objDefEditorMainForm.Designer.cs
@@ -34,23 +34,30 @@
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editFieldToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(13, 38);
+            this.treeView1.Location = new System.Drawing.Point(13, 27);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(653, 807);
+            this.treeView1.Size = new System.Drawing.Size(653, 818);
             this.treeView1.TabIndex = 0;
+            this.treeView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseClick);
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.editFieldToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(678, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(679, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -59,6 +66,10 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
             this.toolStripSeparator1,
+            this.toolStripComboBox1,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.toolStripSeparator2,
             this.quitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
@@ -83,15 +94,51 @@
             this.quitToolStripMenuItem.Text = "Quit";
             this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
             // 
+            // editFieldToolStripMenuItem
+            // 
+            this.editFieldToolStripMenuItem.Name = "editFieldToolStripMenuItem";
+            this.editFieldToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.editFieldToolStripMenuItem.Text = "Edit Field";
+            this.editFieldToolStripMenuItem.Click += new System.EventHandler(this.editFieldToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(178, 6);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Enabled = false;
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
             // objDefEditorMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 857);
+            this.ClientSize = new System.Drawing.Size(679, 857);
             this.Controls.Add(this.treeView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(695, 895);
+            this.MinimumSize = new System.Drawing.Size(695, 895);
             this.Name = "objDefEditorMainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -108,6 +155,11 @@
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editFieldToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
     }
 }
 
--- Src/ToolKit/ObjDefEditor/objDefEditorMainForm.cs
+++ Src/ToolKit/ObjDefEditor/objDefEditorMainForm.cs
@@ -14,6 +14,9 @@ namespace ObjDefEditor
 {
     public partial class objDefEditorMainForm : Form
     {
+        private List<string> openFiles;
+        private TreeNode selectedNode;
+
         public TreeNode Json2Tree(dynamic obj)
         {
             TreeNode parent = new TreeNode();
@@ -23,7 +26,7 @@ namespace ObjDefEditor
                 TreeNode child = new TreeNode();
                 child.Text = token.Name.ToString();
 
-                if (token.Value.ToString()[0] != '{' && token.Value.ToString()[0] != '[')
+                if (token.Value.ToString() == "" || (token.Value.ToString()[0] != '{' && token.Value.ToString()[0] != '['))
                 {
                     child.Nodes.Add(token.Value.ToString());
                 }
@@ -42,9 +45,8 @@ namespace ObjDefEditor
                         {
                             TreeNode objTN = new TreeNode();
                             ix++;
-                            if (itm.ToString()[0] != '{' && itm.ToString()[0] != '[')
+                            if (itm.ToString() == "" || (itm.ToString()[0] != '{' && itm.ToString()[0] != '['))
                             {
-                                //child.Nodes.Add(itm.ToString());
                                 objTN.Nodes.Add(itm.ToString());
                             }
                             else
@@ -74,16 +76,16 @@ namespace ObjDefEditor
             {
                 contents = sReader.ReadToEnd();
             }
-            return LoadObjDefS(contents);
+            return LoadObjDefS(contents, filePath);
         }
 
-        public dynamic LoadObjDefS(string s)
+        public dynamic LoadObjDefS(string s, string parentText="Root Object")
         {
             var json = (Newtonsoft.Json.Linq.JObject)
                 JsonConvert.DeserializeObject(s, new JsonSerializerSettings() { TypeNameHandling = TypeNameHandling.Objects });
 
             TreeNode parent = Json2Tree(json);
-            parent.Text = "Root Object";
+            parent.Text = parentText;
 
             return parent;
         }
@@ -95,29 +97,68 @@ namespace ObjDefEditor
             ofd.FilterIndex = 1;
             ofd.Multiselect = false;
             DialogResult userClickedOK = ofd.ShowDialog();
-            if (userClickedOK == DialogResult.OK)
+            if (userClickedOK == DialogResult.OK && !this.openFiles.Contains(ofd.FileName))
             {
                 var obj = LoadObjDef(ofd.FileName);
+                this.openFiles.Add(ofd.FileName);
                 return obj;
+            }
+            else if (this.openFiles.Contains(ofd.FileName))
+            {
+                MessageBox.Show("File already open!\n" + ofd.FileName);
             }
 
             return null;
         }
 
-        public objDefEditorMainForm()
-        {
-            InitializeComponent();
-        }
-
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var obj = LoadObjDefDialog();
-            treeView1.Nodes.Add(obj);
+            if (this.openFiles.Count == 0)
+            {
+                var obj = LoadObjDefDialog();
+                if (obj != null)
+                    treeView1.Nodes.Add(obj);
+            }
         }
 
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void editFieldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.selectedNode != null && this.selectedNode != this.treeView1.Nodes[0])
+            {
+                string edit = this.selectedNode.Text;
+                Dialogs.InputBox("Title", "Prompt", ref edit);
+                this.selectedNode.Text = edit;
+            }
+        }
+
+        private void treeView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.selectedNode = this.treeView1.GetNodeAt(e.X, e.Y);
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.openFiles.Count != 0)
+            {
+                this.treeView1.Nodes.RemoveAt(0);
+                this.openFiles.RemoveAt(0);
+            }
+        }
+
+        public objDefEditorMainForm()
+        {
+            InitializeComponent();
+            this.openFiles = new List<string>();
         }
     }
 }
